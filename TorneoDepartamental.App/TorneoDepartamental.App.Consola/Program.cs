8a6cd310eb177c2294b52bab32f552d8b55ede51
AlexanderAlzate07 <alexander.alzatez@udea.edu.co>
2021-09-22 21:32:56 -0500

    Actualización CRUD equipos

--- TorneoDepartamental.App/TorneoDepartamental.App.Consola/Program.cs
+++ TorneoDepartamental.App/TorneoDepartamental.App.Consola/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using TorneoDepartamental.App.Dominio;
 using TorneoDepartamental.App.Persistencia;
 
@@ -7,10 +8,25 @@ namespace TorneoDepartamental.App.Consola
     class Program
     {
         private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio(new Persistencia.AppContext());
+        private static IRepositorioEquipo _repoEquipo = new RepositorioEquipo(new Persistencia.AppContext());
         static void Main(string[] args)
         {
-            // AddMunicipio("Caldas");
-            Console.WriteLine("Hello World!. Se agregó municipio");
+            //------------------------  CRUD MUNICIPIO----------------
+            // AddMunicipio("Bello");
+            // GetMunicipio(4);
+            // GetAllMunicipios();
+            DeleteMunicipio(8);
+            DeleteMunicipio(9);
+            // UpdateMunicipio(5);
+            //--------------------------------------------------------
+
+            //------------------------  CRUD EQUIPO-------------------
+            // AddEquipo("Juventus",1);
+            AsignarMunicipio(1,1);
+            //--------------------------------------------------------
+
+            Console.WriteLine("Hello World!");
+
         }
 
         private static void AddMunicipio(string nombreMunicipio){
@@ -18,6 +34,36 @@ namespace TorneoDepartamental.App.Consola
                 Nombre = nombreMunicipio
             };
             _repoMunicipio.AddMunicipio(municipio);
+        }
+        private static void GetMunicipio(int idMunicipio){
+           var municipio = _repoMunicipio.GetMunicipio(idMunicipio);
+           System.Console.WriteLine("Nombre del municipio encontrado: "+municipio.Nombre);
+        }
+
+        private static void GetAllMunicipios(){
+            IEnumerable<Municipio> listaMunicipios = _repoMunicipio.GetAllMunicipios();
+            System.Console.WriteLine("Cantidad de municipios: "+listaMunicipios.ToString());
+        }
+        private static void DeleteMunicipio(int idMunicipio){
+            _repoMunicipio.DeleteMunicipio(idMunicipio);
+            System.Console.WriteLine("Se borró el municipio con id: "+idMunicipio);
+        }
+        private static void UpdateMunicipio(int idMunicipio, string nombreNuevo){
+            Municipio municipioEncontrado = _repoMunicipio.GetMunicipio(idMunicipio);
+            municipioEncontrado.Nombre = nombreNuevo;
+            Municipio municipioUpdated = _repoMunicipio.UpdateMunicipio(municipioEncontrado);
+        }
+
+        private static void AddEquipo(string nombreEquipo, int idMunicipio){
+            var municipioEncontrado = _repoMunicipio.GetMunicipio(idMunicipio);
+            System.Console.WriteLine("Municipio encontrado: "+municipioEncontrado.Nombre);
+            var equipo = new Equipo{
+                Nombre = nombreEquipo
+            };
+            _repoEquipo.AddEquipo(equipo);
+        }
+        private static void AsignarMunicipio(int idEquipo, int idMunicipio){
+            _repoEquipo.AsignarMunicipio(idEquipo,idMunicipio);
         }
     }
 }
--- TorneoDepartamental.App/TorneoDepartamental.App.Persistencia/AppRepositorios/RepositorioEquipo.cs
+++ TorneoDepartamental.App/TorneoDepartamental.App.Persistencia/AppRepositorios/RepositorioEquipo.cs
@@ -4,8 +4,54 @@ using System.Linq;
 
 namespace TorneoDepartamental.App.Persistencia
 {
-    public class RepositorioEquipo
+    public class RepositorioEquipo : IRepositorioEquipo
     {
-        
+        private readonly AppContext _appContext = new AppContext();
+        public RepositorioEquipo(AppContext appContext)
+        {
+        }
+
+        public Equipo AddEquipo(Equipo equipo){
+            var equipoAdicionado = _appContext.Equipos.Add(equipo);
+            _appContext.SaveChanges();
+            return equipoAdicionado.Entity;
+        }
+        public Equipo UpdateEquipo(Equipo equipo){
+            var equipoEncontrado = _appContext.Equipos.FirstOrDefault(e => e.Id == equipo.Id);
+            if(equipoEncontrado != null){
+                equipoEncontrado.Nombre = equipo.Nombre;
+                equipoEncontrado.Municipio = equipo.Municipio;
+                _appContext.SaveChanges();
+            }
+            return equipoEncontrado;
+        }
+        public void DeleteEquipo(int idEquipo){
+            var equipoEncontrado = _appContext.Equipos.FirstOrDefault(e => e.Id == idEquipo);
+            if(equipoEncontrado == null)
+                return;
+            _appContext.Equipos.Remove(equipoEncontrado);
+            _appContext.SaveChanges();
+        }
+        public Equipo GetEquipo(int idEquipo){
+            return _appContext.Equipos.FirstOrDefault(e => e.Id == idEquipo);
+        }
+        public IEnumerable<Equipo> GetAllEquipos(){
+            return _appContext.Equipos;
+        }
+        public Municipio AsignarMunicipio(int idEquipo,int idMunicipio){
+            var equipoEncontrado = _appContext.Equipos.FirstOrDefault(e => e.Id == idEquipo);
+            if(equipoEncontrado != null)
+            {
+                var municipioEncontrado = _appContext.Municipios.FirstOrDefault(m => m.Id == idMunicipio);
+                if(municipioEncontrado != null)
+                {
+                    equipoEncontrado.Municipio = municipioEncontrado;
+                    _appContext.SaveChanges();
+                }
+                return municipioEncontrado;
+            }
+            return null;
+        }
+
     }
 }
