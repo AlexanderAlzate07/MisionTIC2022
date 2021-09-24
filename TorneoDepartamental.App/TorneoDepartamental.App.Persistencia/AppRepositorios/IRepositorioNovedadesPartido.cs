9ce248043b58df6dffc24785a63e32a51e855522
AlexanderAlzate07 <alexander.alzatez@udea.edu.co>
2021-09-24 07:53:41 -0500

    corrección de novedades partidos

--- TorneoDepartamental.App/TorneoDepartamental.App.Persistencia/AppRepositorios/IRepositorioNovedadesPartido.cs
+++ TorneoDepartamental.App/TorneoDepartamental.App.Persistencia/AppRepositorios/IRepositorioNovedadesPartido.cs
@@ -3,7 +3,7 @@ using TorneoDepartamental.App.Dominio;
 
 namespace TorneoDepartamental.App.Persistencia
 {
-    public interface RepositorioNovedadesPartido
+    public interface IRepositorioNovedadesPartido
     {
          
     }
--- TorneoDepartamental.App/TorneoDepartamental.App.Persistencia/AppRepositorios/RepositorioNovedadesPartido.cs
+++ TorneoDepartamental.App/TorneoDepartamental.App.Persistencia/AppRepositorios/RepositorioNovedadesPartido.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace TorneoDepartamental.App.Persistencia
 {
-    public class IRepositorioNovedadesPartido
+    public class RepositorioNovedadesPartido
     {
          
     }
