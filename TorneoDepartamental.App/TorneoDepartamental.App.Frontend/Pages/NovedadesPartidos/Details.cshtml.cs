f493f9302b400708f034dcfb1bc5d7328dce1626
AlexanderAlzate07 <alexander.alzatez@udea.edu.co>
2021-09-30 19:08:03 -0500

    Detalles de Municipios, Equipos y Arbitros finalizado

---
+++ TorneoDepartamental.App/TorneoDepartamental.App.Frontend/Pages/NovedadesPartidos/Details.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoDepartamental.App.Persistencia;
+using TorneoDepartamental.App.Dominio;
+
+namespace TorneoDepartamental.App.Frontend.Pages.NovedadesPartidos
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
