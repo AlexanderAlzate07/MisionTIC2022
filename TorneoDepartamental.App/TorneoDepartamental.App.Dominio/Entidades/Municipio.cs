0dcdbc4e125fe6d7d44563dee54ef34b659b2cfd
AlexanderAlzate07 <alexander.alzatez@udea.edu.co>
2021-10-05 07:05:59 -0500

    CREATE de Municipios finalizado

--- TorneoDepartamental.App/TorneoDepartamental.App.Dominio/Entidades/Municipio.cs
+++ TorneoDepartamental.App/TorneoDepartamental.App.Dominio/Entidades/Municipio.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
 namespace TorneoDepartamental.App.Dominio
 {
     public class Municipio
     {
         public int Id { get; set; }
+        [Display(Name="Nombre del municipio")]
         public string Nombre { get; set; }
     }
 }
