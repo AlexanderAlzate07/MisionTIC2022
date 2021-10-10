44aa4594e07d27e5a420ce8d0da618ae499b444b
cristhian gonzalez <cristhianmg1122@gmail.com>
2021-10-10 18:40:58 -0500

    Terminado la page PArtidos con todas sus asignaciones

--- TorneoDepartamental.App/TorneoDepartamental.App.Frontend/Pages/Partidos/AddArbitro.cshtml.cs
+++ TorneoDepartamental.App/TorneoDepartamental.App.Frontend/Pages/Partidos/AddArbitro.cshtml.cs
@@ -1,16 +1,37 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using TorneoDepartamental.App.Dominio;
+using TorneoDepartamental.App.Persistencia;
 
 namespace TorneoDepartamental.App.Frontend.Pages.Partidos
 {
     public class AddArbitroModel : PageModel
     {
-        public void OnGet()
+        public readonly IRepositorioPartido _repoPartido;
+        public readonly IRepositorioArbitro _repoArbitro;
+        public Partido partido {get;set;}
+        public IEnumerable<Arbitro>arbitros {get;set;}
+        public AddArbitroModel(IRepositorioPartido repoPartido, IRepositorioArbitro repoArbitro)
         {
+            _repoPartido = repoPartido;
+            _repoArbitro = repoArbitro;
+        }
+        public void OnGet(int id)
+        {
+            partido = _repoPartido.GetPartido(id);
+            arbitros = _repoArbitro.GetAllArbitros();
+        }
+
+        public IActionResult OnPost(int idPartido, int idArbitro)
+        {
+            _repoPartido.AsignarArbitro(idPartido, idArbitro);
+            return RedirectToPage("Index");
         }
     }
 }
--- TorneoDepartamental.App/TorneoDepartamental.App.Frontend/Pages/Partidos/AddEquipoVisitante.cshtml.cs
+++ TorneoDepartamental.App/TorneoDepartamental.App.Frontend/Pages/Partidos/AddEquipoVisitante.cshtml.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoDepartamental.App.Dominio;
+using TorneoDepartamental.App.Persistencia;
 
 namespace TorneoDepartamental.App.Frontend.Pages.Partidos
 {
     public class AddEquipoVisitanteModel : PageModel
     {
-        public void OnGet()
+         private readonly IRepositorioPartido _repoPartido;
+        private readonly IRepositorioEquipo _repoEquipo;
+        public Partido partido { get; set; }
+        public IEnumerable<Equipo> equipos { get; set; }
+        public AddEquipoVisitanteModel(IRepositorioPartido repoPartido, IRepositorioEquipo repoEquipo)
         {
+            _repoPartido = repoPartido;
+            _repoEquipo = repoEquipo;
+        }
+        public void OnGet(int id)
+        {
+            partido = _repoPartido.GetPartido(id);
+            equipos = _repoEquipo.GetAllEquipos();
+
+        }
+
+        public IActionResult OnPost(int idPartido, int idEquipo)
+        {
+            _repoPartido.AsignarEquipoVisitante(idPartido, idEquipo);
+            return RedirectToPage("Index");
         }
     }
 }
