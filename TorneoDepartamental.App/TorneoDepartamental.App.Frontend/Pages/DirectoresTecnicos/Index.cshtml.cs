703e2cc095b3b4ac689c9a34d694e06319451c53
cristhian gonzalez <cristhianmg1122@gmail.com>
2021-10-18 01:18:41 -0500

    Se fusiono la rama autenticacion con cristhian y queda implementado el login en la aplicacion

--- TorneoDepartamental.App/TorneoDepartamental.App.Frontend/Pages/DirectoresTecnicos/Index.cshtml.cs
+++ TorneoDepartamental.App/TorneoDepartamental.App.Frontend/Pages/DirectoresTecnicos/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,9 +11,9 @@ using TorneoDepartamental.App.Persistencia;
 
 namespace TorneoDepartamental.App.Frontend.Pages.DirectoresTecnicos
 {
+    [Authorize]
     public class IndexModel : PageModel
-    {
-        
+    {     
         private readonly IRepositorioDirectorTecnico _repoTecnico;
         public IEnumerable<DirectorTecnico> directorTecnicos { get; set; }
         public string bActual {get;set;}
--- TorneoDepartamental.App/TorneoDepartamental.App.Frontend/Pages/Estadios/Index.cshtml.cs
+++ TorneoDepartamental.App/TorneoDepartamental.App.Frontend/Pages/Estadios/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TorneoDepartamental.App.Dominio;
@@ -9,6 +10,7 @@ using TorneoDepartamental.App.Persistencia;
 
 namespace TorneoDepartamental.App.Frontend.Pages.Estadios
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly IRepositorioEstadio _repoEstadio;
