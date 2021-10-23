0c77e1c5b0fa23d6928ad6cd95d06750dedbdeea
AlexanderAlzate07 <alexander.alzatez@udea.edu.co>
2021-10-22 22:34:34 -0500

    Authorization finalizada

--- TorneoDepartamental.App/TorneoDepartamental.App.Frontend/Pages/Equipos/Index.cshtml.cs
+++ TorneoDepartamental.App/TorneoDepartamental.App.Frontend/Pages/Equipos/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TorneoDepartamental.App.Persistencia;
 using TorneoDepartamental.App.Dominio;
+
 namespace TorneoDepartamental.App.Frontend.Pages.Equipos
 {
     public class IndexModel : PageModel
